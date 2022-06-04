1ad4a442561ec2126e238314be455ff3624f5d82
Elijah Heisenberg <exijab@gmail.com>
2022-06-04 09:51:12 +0300

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0;
+
+  function increment() {
+    count++;
+  }
+</script>
+
+<main>
+  <h1>Number {count}</h1>
+  <p>This is a simple Svelte app.</p>
+  <button on:click={increment}>+1</button>
+</main>
+
+<style>
+</style>
